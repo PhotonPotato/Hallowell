9d9a07305781fefc834fc0d099da288d3a8796cb
PhotonPotato <photonpotato21@gmail.com>
2024-02-19 16:36:25 -1000

    v0.0.8a7 - Scrapped & Added Anxiousness
    Updates
     - Scrapped a lot of the old emotions code (its still there just commented out and unused)
     - Simplified emotions down to axiousness and I think hunger
     - Anxiousness constituted the speed and frequency of little "skitters" where the desiredPos jumps around randomly in a small zone
     - Also see the new indicators for players last pos at lost line of sight (lostLoS)
     - After lostLoS, the enemy will just idle in one position
     - After a specific time, the enemy will "investigate" the last seen pos of the player
    
    Active Bugs
     - See standing on the top most blocks in level 2 while the enemy sits just to the left of the smallest blocks (enemy jitters weirdly. most likely due to the intermitent clearLoS and consequent on and off skitters)

--- Assets/Scripts/Enemies/ProceduralAnimations/BoidfangManager.cs
+++ Assets/Scripts/Enemies/ProceduralAnimations/BoidfangManager.cs
@@ -35,18 +35,37 @@ public class BoidfangManager : MonoBehaviour
  
     [Header("Settings")]
     public float maxBodyMoveSpeed = 9;
-    public float currentSpeedMult = 1;
-    public float hungerRate = .5f;
+    public float currentRawSpeedMult = 1;
+    public float currentSmoothedSpeedMult = 1;
 
-    public float curiosityAdversaryDistanceFloor = 10f;
+    [Space]
+    public float hungerRate = .5f;
+    public float randomPositionWaypointOffsetMaximum = 10;
+    private float currentPositionWaypointOffset = 0;
+    public float minimumDistanceToWaypoint = 1;
+
+    public float timeUntilInvestigation = 5f;
+    public float timeUntilGiveUpInvestigation = 5f;
+
+    //public float curiosityAdversaryDistanceFloor = 10f;
 
     [Header("Trackers")]
     public Vector2 relativePlayerDirection;
     public bool clearLineOfSight = false;
+    bool lostLoS = false;
     public EnemyFeelings BoidfangFeelings;
     public Vector3 desiredStationaryPosition;
+    public Vector3 lastSeenAdversaryPosition;
+    public float timeSinceLostLoS = 0;
+    private Vector3 entityPositionAtLostLoS;
+    private Vector3 initialEntityPos;
 
     [Header("Enemy Reactions")]
+    public float enemyHunger = 0;
+    public float anxiousness = 0;
+    public bool investigating = false;
+
+    #region Depricated E Vars
     public AnimationCurve aggressionVsMovementSpeed;
     public AnimationCurve fearVsMovementSpeed;
     public AnimationCurve curiosityVsMovementSpeed;
@@ -63,6 +82,13 @@ public class BoidfangManager : MonoBehaviour
     public AnimationCurve FearVsDistToAdversary;
 
     public AnimationCurve AdversaryAggressionVsFear;
+    #endregion
+    public AnimationCurve DistToAdversaryVsHunger;
+    public AnimationCurve AnxiousnessVsAnxiousnessRate;
+    public AnimationCurve AnxiousnessVsSkitterSpeed;
+
+    public GameObject lastSeenPosMarker;
+    public Transform myPosLastSeen;
 
     private void Start()
     {
@@ -72,11 +98,16 @@ public class BoidfangManager : MonoBehaviour
         PlayerManager = FindObjectOfType<PlayerManager>();
 
         BoidfangFeelings.init();
+
+        initialEntityPos = transform.position;
     }
 
     public void Update()
     {
         if (dead) return;
+
+        lastSeenPosMarker.transform.position = lastSeenAdversaryPosition;
+        myPosLastSeen.position = entityPositionAtLostLoS;
 
         //Check for death
         if(m_healthContainer.currentHealth <= 0)
@@ -85,23 +116,49 @@ public class BoidfangManager : MonoBehaviour
             return;
         }
 
-        EvaluateAdversaryAggression();
-        EvaluateCuriosity();
-        EvaluateEmotions();
+        //Check for investigating condition
+        investigating = !clearLineOfSight && timeSinceLostLoS >= timeUntilInvestigation && timeSinceLostLoS < timeUntilGiveUpInvestigation;
+
+        //EvaluateAdversaryAggression();
+        //EvaluateCuriosity();
+        //EvaluateEmotions();
         CalcDesiredPos();
 
         //Pathfinding
         PathfindToPlayer();
 
+        //Update emotions
+        if (lostLoS)
+        {
+            //Make a curve so it gets more anxious exponentially
+            anxiousness += AnxiousnessVsAnxiousnessRate.Evaluate(anxiousness);
+
+            lastSeenAdversaryPosition = Adversary.position;
+            timeSinceLostLoS = 0;
+
+            //Update the desiredPos type ting
+            entityPositionAtLostLoS = transform.position;
+
+            //Reset lostLoS
+            lostLoS = false;
+        }
+
         //Update body position
         Vector3 newPos = transform.position;
 
         //Made in desmos
-        float distanceFalloffMultiplier = -1 * Mathf.Pow(.5f, Mathf.Clamp(Mathf.Abs(desiredStationaryPosition.x - transform.position.x), 0, 100)) + 1;
+        float distanceFalloffMultiplier = -1 * Mathf.Pow(.5f, Mathf.Max(Mathf.Abs(desiredStationaryPosition.x - transform.position.x), 0)) + 1;
+
+        //Smooth out the speed mult
+        currentSmoothedSpeedMult += (currentRawSpeedMult - currentSmoothedSpeedMult) / 10;
 
         //Update the body x
-        newPos.x += maxBodyMoveSpeed * Mathf.Sign(desiredStationaryPosition.x - transform.position.x) * currentSpeedMult *
-                    distanceFalloffMultiplier * EaseOutQuint((float)animationScript.currentBoidCountTotal / animationScript.startingTotalBoids) * Time.deltaTime;
+        newPos.x += maxBodyMoveSpeed
+                    * Mathf.Sign(desiredStationaryPosition.x - transform.position.x)
+                    * currentSmoothedSpeedMult
+                    * distanceFalloffMultiplier
+                    * EaseOutQuint((float)animationScript.currentBoidCountTotal / animationScript.startingTotalBoids)
+                    * Time.deltaTime;
 
         newPos.y += (animationScript.CalculateBodyHeight(relativePlayerDirection.x > 0 ? 0 : 5, 20) - newPos.y) / 10;
 
@@ -145,7 +202,7 @@ public class BoidfangManager : MonoBehaviour
     public void PathfindToPlayer()
     {
         //Move in direciton
-        relativePlayerDirection = (PlayerManager.transform.position - transform.position).normalized;
+        relativePlayerDirection = (Adversary.position - transform.position).normalized;
 
         //Clear line of sight
         RaycastHit2D[] hits = Physics2D.RaycastAll((Vector2) transform.position + (relativePlayerDirection * 2), relativePlayerDirection);
@@ -154,13 +211,44 @@ public class BoidfangManager : MonoBehaviour
         {
             if (hit.transform.tag == "Ground" || hit.transform.gameObject.layer == LayerMask.GetMask("Wall"))
             {
+                //Detect a loss of LoS (clear LoS went from true to false
+                if (clearLineOfSight) lostLoS = true;
+
                 clearLineOfSight = false;
+
+                //Upadate timers
+                timeSinceLostLoS += Time.deltaTime;
                 break;
             }else if (hit.transform.tag == "Player")
             {
                 clearLineOfSight = true;
                 break;
             }
+        }
+
+        if (clearLineOfSight && Vector3.Distance(transform.position, Adversary.position) <= 50)
+        {
+            enemyHunger += hungerRate * Time.deltaTime;
+        }
+
+        float distToDesiredPos = Mathf.Abs(transform.position.x - desiredStationaryPosition.x);
+
+        if (distToDesiredPos <= minimumDistanceToWaypoint)
+        {
+            //Then find a new Waypoint offset (within specified range)
+            currentPositionWaypointOffset = randomPositionWaypointOffsetMaximum * Random.Range(-1f, 1f);
+
+            if (investigating) currentPositionWaypointOffset *= Random.Range(1f, 2f);
+        }
+
+        //Arbitrary value of 1.3
+        if (distToDesiredPos <= randomPositionWaypointOffsetMaximum * (investigating ? 2.4f : 1.3f))
+        {
+            currentRawSpeedMult = AnxiousnessVsSkitterSpeed.Evaluate(anxiousness);
+        }
+        else
+        {
+            currentRawSpeedMult = 1f;
         }
     }
 
@@ -169,7 +257,8 @@ public class BoidfangManager : MonoBehaviour
         return 1 - Mathf.Pow(1 - x, 5);
     }
 
-    public void EvaluateAdversaryAggression()
+    #region Depricated code
+    /*public void EvaluateAdversaryAggression()
     {
         //Calculate the aggression of an adversary (from 0 to 1) using only the transform and rigidbody
 
@@ -190,7 +279,8 @@ public class BoidfangManager : MonoBehaviour
         BoidfangFeelings.adversaryAggression = Mathf.Clamp(BoidfangFeelings.adversaryAggression, 0, 1);
     }
 
-    public void EvaluateCuriosity()
+    //CLEAN THIS PLEASE
+    /*public void EvaluateCuriosity()
     {
         //Maybe check the distance to player is far away?
         if (!clearLineOfSight)
@@ -205,9 +295,9 @@ public class BoidfangManager : MonoBehaviour
         BoidfangFeelings.curiosity += Mathf.Pow(.04f, Vector3.Distance(transform.position, Adversary.position) - curiosityAdversaryDistanceFloor) * Time.deltaTime;
 
 
-    }
+    }*/
 
-    public void EvaluateEmotions()
+    /*public void EvaluateEmotions()
     {
         //Evaluate FeAr
         float currentFear = healthVsFear.Evaluate(m_healthContainer.currentHealth / m_healthContainer.maxHealth) * ((float)2 / 3)
@@ -238,16 +328,28 @@ public class BoidfangManager : MonoBehaviour
 
         BoidfangFeelings.hunger += hungerRate * Time.deltaTime;
         BoidfangFeelings.hunger = Mathf.Clamp(BoidfangFeelings.hunger, 0, 1);
-    }
+    }*/
+    #endregion
 
     public void CalcDesiredPos()
     {
-        //
-        Vector3 desiredPos = Adversary.position;
+        //Calc the desired distance
+        BoidfangFeelings.desiredDistToEnemy = Mathf.Max(DistToAdversaryVsHunger.Evaluate(Mathf.Min(enemyHunger, 1)), 6);
+        Vector3 desiredPos = (!clearLineOfSight && !investigating) ? entityPositionAtLostLoS : Adversary.position;
+        if (investigating) desiredPos = lastSeenAdversaryPosition;
+        else if (timeSinceLostLoS < timeUntilGiveUpInvestigation && timeSinceLostLoS > timeUntilInvestigation) desiredPos = initialEntityPos;
 
         //Subtract because relPlayerDir points toward the adversary from this obj and we want the opposite.
-        desiredPos -= (Vector3) relativePlayerDirection * BoidfangFeelings.desiredDistToEnemy;
+        //ONLY IF it can see the adversary, otherwise just idle
+        if (clearLineOfSight) desiredPos -= (Vector3) relativePlayerDirection * BoidfangFeelings.desiredDistToEnemy;
+
+        desiredPos.x += currentPositionWaypointOffset;
 
         desiredStationaryPosition = desiredPos;
+    }
+
+    private void OnAttack()
+    {
+        
     }
 }
